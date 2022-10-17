58721baf6e409ecb0d378634314fe370c0c5df1d
lucas <lucaspetrocinireis@hotmail.com>
2022-10-17 20:03:15 -0300

    Starting to implement Board and Piece

---
+++ src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import boardgame.Board;
+import boardgame.Position;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Board board = new Board(8, 8);
+
+    }
+
+}
