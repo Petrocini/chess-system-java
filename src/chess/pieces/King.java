f7aaa89962f76b68ce55f3de93c32c9a8601a783
lucas <lucaspetrocinireis@hotmail.com>
2022-11-17 19:33:35 -0300

    Switching player each turn

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -66,6 +66,7 @@ public class King extends ChessPiece {
         if (getBoard().positionExists(p) && canMove(p)) {
             mat[p.getRow()][p.getColumn()] = true;
         }
+
         return mat;
     }
 }
